b3e092a61c419facb9ed86a6578e266948c75c00
Yuval Drori <yuvald29@protonmail.com>
2021-11-25 17:18:23 +0100

    all tests passing

--- contracts/NollaCoin.sol
+++ contracts/NollaCoin.sol
@@ -4,12 +4,21 @@ pragma solidity >0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NollaCoin is ERC20 {
-	constructor(uint256 initialSupply) ERC20("NollaCoin", "NOL") {
-		_mint(msg.sender, initialSupply * 10 ** decimals());
-	}
+  mapping(address => mapping(address => uint256)) public barksLedger; // Key is user who has been barked upon. Value is a mapping of users who barked on the user, and how many times each user barked on them.
+  uint256 public barksCost = 1000;
 
-	function decimals() pure public override returns(uint8) {
-		return 20;
-	} 
+  constructor(uint256 initialSupply) ERC20("NollaCoin", "NOL") {
+    _mint(msg.sender, initialSupply * 10**decimals());
+  }
 
+  function decimals() public pure override returns (uint8) {
+    return 20;
+  }
+
+  function bark(address barkee, uint8 barksAmount) external {
+    require(balanceOf(msg.sender) > (barksCost * barksAmount)); // Barker must have 1000 NollaCoin wei per bark to bark at other users
+
+    mapping(address => uint256) storage barkersOnUserList = barksLedger[barkee]; // Get full list of users who barked on the barkee
+    barkersOnUserList[msg.sender] = barkersOnUserList[msg.sender] + barksAmount; // Go the "row" where the amount of times the barker barked upon the barkee is, and increment it by 1
+  }
 }
--- test/nollacoin.js
+++ test/nollacoin.js
@@ -2,20 +2,106 @@ const NollaCoin = artifacts.require("NollaCoin");
 
 let instance;
 
-beforeEach(async function() {
-    instance = await NollaCoin.deployed();
-})
+beforeEach(async function () {
+  instance = await NollaCoin.new(1000);
+});
 
-contract('NollaCoin', (accounts) => {
+contract("NollaCoin", (accounts) => {
+  describe("basic erc-20 tests", () => {
     it("should have 20 decimals", async () => {
-        const decimals = await instance.decimals();
+      const decimals = await instance.decimals();
 
-        assert.equal(decimals, 20)
+      assert.equal(decimals, 20);
     });
 
-  it(`should put ${1000*10**20} NollaCoin in the first account`, async () => {
-    const balance = await instance.balanceOf.call(accounts[0]);
+    it(`should put ${
+      1000 * 10 ** 20
+    } NollaCoin in the first account`, async () => {
+      const balance = await instance.balanceOf.call(accounts[0]);
 
-    assert.equal(balance.valueOf().toString(), 1000*10**20);
+      assert.equal(balance.valueOf().toString(), 1000 * 10 ** 20);
+    });
+
+    it("should transfer 1000 NollaCoin wei from account 0 to account 1", async () => {
+      let balance = await instance.balanceOf.call(accounts[1]);
+      balance = balance.valueOf().toString();
+      assert.equal(balance, 0, "Starting balance is not 0");
+
+      await instance.transfer(accounts[1], 1000, {from: accounts[0]});
+      balance = await instance.balanceOf.call(accounts[1]);
+      balance = balance.valueOf().toString();
+
+      assert.equal(balance, 1000, "Final balance is not 1000");
+    });
+  });
+
+  describe("bark tests", () => {
+    it("should fail if barker doesn't have enough wei to bark", async () => {
+      try {
+        let balance = await instance.balanceOf.call(accounts[1]);
+        balance = balance.valueOf().toString();
+        assert.equal(balance, 0, "Starting balance is not 0");
+
+        await bark(instance, accounts[1], accounts[0], 1);
+      } catch (err) {
+        assert.ok(
+          err,
+          "Barking is succeeding despite user not having enough wei"
+        );
+      }
+
+      try {
+        const barksCost = await instance.barksCost();
+        let balance = await instance.balanceOf.call(accounts[0]);
+        balance = balance.valueOf().toString();
+
+        assert.isBelow(
+          balance,
+          barksCost * 9999999999,
+          "User has enough wei to bark, test invalid"
+        );
+
+        await bark(instance, accounts[0], accounts[1], 9999999999);
+      } catch (err) {
+        assert.ok(
+          err,
+          "Barking is succeeding despite user not having enough wei"
+        );
+      }
+    });
+
+    it("should increase barks amount from accounts[0] at account[1] to 3", async () => {
+      let barksOn1By0 = await instance.barksLedger(accounts[1], accounts[0]);
+      assert.equal(barksOn1By0, 0, "Starting barks are not 0");
+
+      barksOn1By0 = await bark(instance, accounts[0], accounts[1], 3);
+
+      assert.equal(barksOn1By0, 3, "Final barks are not 3");
+    });
+
+    it("should accumulate barks from accounts[0] at account[1] when bark is called multiple times", async () => {
+      let barksOn1By0 = await instance.barksLedger(accounts[1], accounts[0]);
+      assert.equal(barksOn1By0, 0, "Starting barks are not 0");
+
+      barksOn1By0 = await bark(instance, accounts[0], accounts[1], 2);
+      assert.equal(barksOn1By0, 2, "Barks amount after initial bark is not 2");
+
+      barksOn1By0 = await bark(instance, accounts[0], accounts[1], 3);
+      assert.equal(barksOn1By0, 5, "Final barks amount is not 2 + 3 = 5");
+    });
   });
 });
+
+async function getBalance(instance, account) {
+  let balance = await instance.balanceOf.call(account);
+  balance = balance.valueOf().toString();
+
+  return balance;
+}
+
+async function bark(instance, barker, barkee, amount) {
+  await instance.bark(barkee, amount, {from: barker});
+  const barksCount = await instance.barksLedger(barkee, barker);
+
+  return barksCount.valueOf().toString();
+}
